1ceb7c2cc205a4d34ef0630739913d1c3c7b8d43
aiqingr <ntyaiqingr@gmail.com>
2019-11-18 22:53:33 -0500

    single linked list created

---
+++ linkedList/singleLinkedList.js
@@ -0,0 +1,72 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SingleLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop(){
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current
+    }
+    shift(){
+        if(!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return currentHead
+    }
+    unshift(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = nowNode;
+            this.tail = this.head
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+}
+
+let list = new SingleLinkedList()
+
+list.push(100);
+list.push(201);
+list.push(301);
+list.push(401);
+list.push(501);
