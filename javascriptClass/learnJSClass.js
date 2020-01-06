6ca92ce624aeb263ed89d15d0b322fcf66f2d10e
Tongyang Ni <ntyaiqingr@gmail.com>
2020-01-05 22:13:32 -0500

    class key word

--- javascriptClass/learnJSClass.js
+++ javascriptClass/learnJSClass.js
@@ -1,32 +1,20 @@
-class Student{
-    constructor(firstName, lastName, year){
+class Student {
+    constructor(firstName, lastName, year) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.grade = year;
-        this.tardies = 0;
-        this.scores = [];
-    };
+        this.absence = 0;
+    }
+
     fullName(){
-        return `Your full name is ${this.firstName} ${this.lastName}`;
-    };
-    markLate(){
-        this.tardies += 1;
-        if(this.tardies >= 3){
-            return "YOU ARE EXPELLED!!!!"
-        };
-        return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
-    };
-    addScores(scores){
-        this.scores.push(scores);
-        return this.scores;
-    };
-    calculateAverage(){
-        let sum = this.scores.reduce(function(a,b){return a+b});
-        return sum/this.scores.length;
-    };
-};
+        return `My name is ${this.firstName} ${this.lastName}.`;
+    }
 
-let firstStudent = new Student("Nick", "Ni", 8);
-let secondStudent = new Student("Tian", "Chen", 7);
+    getAbsence(){
+        this.absence += 1;
+        return `${this.firstName} ${this.lastName} has been absence for ${this.absence} time(s).`
+    }
+}
 
-
+let firstStudent = new Student("Tom", "Holland", 1);
+let secondStudent = new Student("Jam", "Tim", 5);
